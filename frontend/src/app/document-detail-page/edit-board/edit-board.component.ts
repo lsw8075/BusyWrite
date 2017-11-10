7730c16055934997e53cf6a0bb987bb61cb88da3
yuyupopo <yuyupopo@snu.ac.kr>
2017-11-10 22:50:38 +0900

    show memos and allow editing different memos

--- frontend/src/app/document-detail-page/edit-board/edit-board.component.ts
+++ frontend/src/app/document-detail-page/edit-board/edit-board.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DragulaService } from 'ng2-dragula/ng2-dragula';
 
 @Component({
   selector: 'app-edit-board',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditBoardComponent implements OnInit {
 
-  constructor() { }
+  memos: Array<any>;
+
+  constructor(private _dragulaService: DragulaService) { }
 
   ngOnInit() {
+    this.memos = [1, 2, 3, 4];
+    this._dragulaService.setOptions('note-bag', {
+      moves: function (el, container, handle) {
+        return handle.className === 'handle';
+      }
+    });
   }
 
 }
--- frontend/src/app/document-detail-page/edit-board/note-view/note-view.component.ts
+++ frontend/src/app/document-detail-page/edit-board/note-view/note-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-note-view',
@@ -7,20 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoteViewComponent implements OnInit {
 
-  public editor;
-  public editorContent = `<h3>I am Example content</h3>`;
-  public editorOptions = {
-    placeholder: 'insert content...'
+  editor;
+  @Input() editorContent = '';
+  @Input() editorOptions = {
+    placeholder: `Write Notes Freely! You can even drag around notes`,
+    scrollingContainer: '.note',
+    theme: 'bubble',
+    // modules: {
+    //   toolbar: [
+    //     [{ header: [1, 2, false] }],
+    //     ['bold', 'italic', 'underline'],
+    //     ['image', 'code-block']
+    //   ]
+    // },
   };
 
   constructor() {}
 
   ngOnInit() {
-    setTimeout(() => {
-      this.editorContent = '<h1>content changed!</h1>';
-      console.log('you can use the quill instance object to do something', this.editor);
-      // this.editor.disable();
-    }, 2800);
   }
 
   onEditorBlured(quill) {
@@ -39,7 +43,5 @@ export class NoteViewComponent implements OnInit {
   onContentChanged({ quill, html, text }) {
     console.log('quill content is changed!', quill, html, text);
   }
-
-
 }
 
