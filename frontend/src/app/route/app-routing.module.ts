19c4cfd35c73cbda5074489ee7ac1a5061db6657
yuyupopo <yuyupopo@snu.ac.kr>
2017-11-12 11:02:21 +0900

    routing implemented issue #28

--- frontend/src/app/route/app-routing.module.ts
+++ frontend/src/app/route/app-routing.module.ts
@@ -1,9 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { SigninComponent } from '../signin/signin.component';
+import { LandingComponent } from '../landing/landing.component';
+import { AlarmPageComponent } from '../alarm-page/alarm-page.component';
+import { DocumentDetailPageComponent } from '../document-detail-page/document-detail-page.component';
+import { MainPageComponent } from '../main-page/main-page.component';
+
 import { AuthenticationService } from '../service/authentication.service';
 
-const routes = [];
+const routes = [
+  { path: '', redirectTo: '/landing', pathMatch: 'full' },
+  { path: 'landing', component: LandingComponent },
+  { path: 'sign_in',  component: SigninComponent },
+  {
+    path: 'alarm',
+    component: AlarmPageComponent,
+    canActivate: [ AuthenticationService ],
+  },
+  {
+    path: 'document/:id',
+    component: DocumentDetailPageComponent,
+    canActivate: [ AuthenticationService ],
+  },
+  {
+    path: 'main',
+    component: MainPageComponent,
+    canActivate: [ AuthenticationService ],
+  },
+  {
+    path: '**',
+    component: LandingComponent,
+    canActivate: [ AuthenticationService ],
+  }
+];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
--- frontend/src/app/service/authentication.service.ts
+++ frontend/src/app/service/authentication.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+
 import { User } from '../model/user';
 
 @Injectable()
-export class AuthenticationService {
+export class AuthenticationService implements CanActivate {
 
+  canActivate(): boolean {
+    return true;
+  }
 }
