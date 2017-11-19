81ba50037a700b6bf01d1d7a6467e01dbb62b6f8
yuyupopo <yuyupopo@snu.ac.kr>
2017-11-19 22:10:58 +0900

    document detail page unit test

--- frontend/src/app/document-detail-page/document-detail-page.component.spec.ts
+++ frontend/src/app/document-detail-page/document-detail-page.component.spec.ts
@@ -1,5 +1,9 @@
 import { ComponentFixture, TestBed, async, fakeAsync, tick } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 import { DocumentDetailPageComponent } from './document-detail-page.component';
 import { DialogModule } from 'primeng/primeng';
 import { EditorModule } from 'primeng/primeng';
@@ -10,7 +14,13 @@ import { Component } from '@angular/core';
 import { DocumentService } from '../service/document.service';
 
 class MockDocumentService {
+  getTitle() {
 
+  }
+
+  setTitle() {
+
+  }
 }
 @Component({
   selector: 'app-view-board',
@@ -36,9 +46,18 @@ class MockEditBoardComponent {
 
 }
 
+@Component({
+  selector: 'app-sangjun-board',
+  template: `<p>mock edit board component</p>`
+})
+class MockSangjunBoardComponent {
+
+}
+
 describe('DocumentDetailPageComponent', () => {
     let comp: DocumentDetailPageComponent;
     let fixture: ComponentFixture<DocumentDetailPageComponent>;
+    let documentService: DocumentService;
 
     beforeEach(async(() => {
       TestBed.configureTestingModule({
@@ -46,13 +65,15 @@ describe('DocumentDetailPageComponent', () => {
           DocumentDetailPageComponent,
           MockViewBoardComponent,
           MockBoardManagerComponent,
-          MockEditBoardComponent
+          MockEditBoardComponent,
+          MockSangjunBoardComponent
         ],
         imports: [
           DialogModule,
           EditorModule,
           ButtonModule,
-          FormsModule
+          FormsModule,
+          BrowserAnimationsModule
         ],
         providers: [
           { provide: DocumentService, useClass: MockDocumentService },
@@ -63,6 +84,7 @@ describe('DocumentDetailPageComponent', () => {
     beforeEach(() => {
       fixture = TestBed.createComponent(DocumentDetailPageComponent);
       comp = fixture.componentInstance;
+      documentService = fixture.debugElement.injector.get(DocumentService);
     });
 
     it('can instantiate it', () => {
@@ -73,4 +95,47 @@ describe('DocumentDetailPageComponent', () => {
       expect(comp).toBeTruthy();
     });
 
+    it('should make expected calls on ngInit', fakeAsync(() => {
+      const title = 'title';
+      spyOn(documentService, 'getTitle').and.returnValue(Promise.resolve(title));
+      comp.ngOnInit();
+      tick();
+      expect(documentService.getTitle).toHaveBeenCalled();
+      expect(comp.documentTitle).toBe(title);
+    }));
+
+    describe('after onInit is called', () => {
+      beforeEach( async(() => {
+        const title = 'title';
+        spyOn(documentService, 'getTitle').and.returnValue(Promise.resolve(title));
+        comp.ngOnInit();
+      }));
+
+      it('should show edit diaglog when click on document title', fakeAsync(() => {
+        const button = fixture.debugElement.query(By.css('#document_title'));
+        button.nativeElement.click();
+        tick();
+        expect(comp.displayTitleEditDialog).toBeTruthy();
+      }));
+
+      it('should change title', () => {
+        comp.documentTitle = 'newTitle';
+        comp.onChangeTitleButton();
+        expect(comp.documentTitle).toBe('newTitle');
+      });
+
+      it('should not change title if new title is empty', fakeAsync(() => {
+        comp.documentTitle = '';
+        comp.onChangeTitleButton();
+        tick();
+        expect(comp.documentTitle).toBe('title');
+      }));
+
+      it('should be able to cancel title change', fakeAsync(() => {
+        comp.documentTitle = 'newTitle';
+        comp.onCancelTitleButton();
+        tick();
+        expect(comp.documentTitle).toBe('title');
+      }));
+    });
 });
