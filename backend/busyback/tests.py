a94a54875f13843fd41c906dec4c537a8609a9df
Dajung Je <ss01363@naver.com>
2017-12-02 03:26:38 -0800

    Fix test to run without error under postgresql

--- backend/busyback/tests.py
+++ backend/busyback/tests.py
@@ -45,20 +45,24 @@ class ChannelConnectTestCase(ChannelTestCase):
 class ChannelReceiveTestCaseOne(ChannelTestCase):
     def setUp(self):
         d1 = Document.objects.create(title='A')
-        b1 = NormalBubble.objects.create(location=0, document = d1, timestamp=timezone.now())
+        b1 = NormalBubble.objects.create(location=0, document = d1)
         u1 = User.objects.create_user(username='swpp')
         u1.set_password('swpp')
         u1.save()
         d1.contributors.add(u1)
         d1.save()
+
+        self.fstid = d1.id
         
         d2 = Document.objects.create(title='B')
-        b2 = NormalBubble.objects.create(location=0, document = d2, timestamp=timezone.now())
+        b2 = NormalBubble.objects.create(location=0, document = d2)
         u2 = User.objects.create_user(username='gonssam')
         u2.set_password('gonssam')
         u2.save()
         d2.contributors.add(u2)
         d2.save()
+
+        self.sndid = d2.id
  
         self.client = HttpClient()
         self.client.login(username='swpp', password='swpp')
@@ -78,7 +82,7 @@ class ChannelReceiveTestCaseOne(ChannelTestCase):
         self.assertEqual(result['body'], 'header and body needed')
 
     def test_wr_receive_header_empty_error(self):
-        message = {"header":"", "body":{"document":"1"}}
+        message = {"header":"", "body":{"document": str(self.fstid)}}
         self.client.send('websocket.receive', content={'order':0}, text=str(message))
         self.client.consume('websocket.receive')
         result = self.client.receive()
@@ -96,7 +100,7 @@ class ChannelReceiveTestCaseOne(ChannelTestCase):
         self.assertEqual(result['body'], 'empty body')
  
     def test_attempt_close_document_before_open_document(self):
-        message = {"header": "close_document", "body": {"document_id": "1"}}
+        message = {"header": "close_document", "body": {"document_id": str(self.fstid)}}
         self.client.send("websocket.receive", content={'order':0}, text=str(message))
         self.client.consume('websocket.receive')
         result = self.client.receive()
@@ -115,7 +119,7 @@ class ChannelReceiveTestCaseOne(ChannelTestCase):
         self.assertEqual(result['body'], 'document_id needed')
 
     def test_open_document_Document_Does_Not_Exist(self):
-        message = {'header': 'open_document', 'body': {'document_id':3}}
+        message = {'header': 'open_document', 'body': {'document_id':0}}
         self.client.send('websocket.receive', content={'order':0}, text=str(message))
         self.client.consume('websocket.receive')
         result = self.client.receive()
@@ -124,7 +128,7 @@ class ChannelReceiveTestCaseOne(ChannelTestCase):
         self.assertEqual(result['body'], 'document does not exist')
 
     def test_open_document_Not_Contributed_By(self):
-        message = {'header': 'open_document', 'body': {'document_id':2}}
+        message = {'header': 'open_document', 'body': {'document_id':str(self.sndid)}}
         self.client.send('websocket.receive', content={'order':0}, text=str(message))
         self.client.consume('websocket.receive')
         result = self.client.receive()
@@ -133,15 +137,15 @@ class ChannelReceiveTestCaseOne(ChannelTestCase):
         self.assertEqual(result['body'], 'user does not contribute to the document')
 
     def test_open_document_success(self):
-        message = {'header': 'open_document', 'body': {'document_id':1}}
+        message = {'header': 'open_document', 'body': {'document_id':str(self.fstid)}}
         self.client.send('websocket.receive', content={'order':0}, text=str(message))
         self.client.consume('websocket.receive')
         result = self.client.receive()
         self.assertEqual(result['header'], 'open_document')
         self.assertEqual(result['accept'], 'True')
-        self.assertEqual(result['body']['document_id'], 1)
+        self.assertEqual(result['body']['document_id'], str(self.fstid))
 
-        Group("document_detail-1").send({"body":"wow"})
+        Group("document_detail-"+str(self.fstid)).send({"body":"wow"})
         result = self.client.receive()
         self.assertEqual(result['body'], 'wow')
 
@@ -150,21 +154,24 @@ class ChannelReceiveTestCaseOne(ChannelTestCase):
 class ChannelReceiveTestCaseTwo(ChannelTestCase):
     def setUp(self):
         d1 = Document.objects.create(title='A')
-        b1 = NormalBubble.objects.create(location=0, document = d1, timestamp=timezone.now())
+        b1 = NormalBubble.objects.create(location=0, document = d1)
         u1 = User.objects.create_user(username='swpp')
         u1.set_password('swpp')
         u1.save()
         d1.contributors.add(u1)
         d1.save()
 
+        self.fstid = d1.id
+        self.bubbleid = b1.id
+
         d2 = Document.objects.create(title='B')
-        b2 = NormalBubble.objects.create(location=0, document = d2, timestamp=timezone.now())
+        b2 = NormalBubble.objects.create(location=0, document = d2)
         u2 = User.objects.create_user(username='gonssam')
         u2.set_password('gonssam')
         u2.save()
         d2.contributors.add(u2)
         d2.save()
- 
+    
         d1.contributors.add(u2)
         d1.save()
 
@@ -175,7 +182,7 @@ class ChannelReceiveTestCaseTwo(ChannelTestCase):
         self.client.send_and_consume("websocket.connect", content=message)
         self.client.receive()
 
-        message = {'header': 'open_document', 'body': {'document_id':'1'}}
+        message = {'header': 'open_document', 'body': {'document_id': str(d1.id)}}
         self.client.send('websocket.receive', content={'order': 0}, text=str(message))
         self.client.consume('websocket.receive')
         self.client.receive()
@@ -210,13 +217,13 @@ class ChannelReceiveTestCaseTwo(ChannelTestCase):
         self.assertEqual(result['body'], 'attempt to close unopened document')
 
     def test_close_document_success(self):
-        message = {'header': 'close_document', 'body': {'document_id':'1'}}
+        message = {'header': 'close_document', 'body': {'document_id':str(self.fstid)}}
         self.client.send('websocket.receive', content={'order':1}, text=str(message))
         self.client.consume('websocket.receive')
         result = self.client.receive()
         self.assertEqual(result['header'], 'close_document')
         self.assertEqual(result['accept'], 'True')
-        self.assertEqual(result['body']['document_id'], '1')
+        self.assertEqual(result['body']['document_id'], str(self.fstid))
 
         # TODO: solve this problem: 
         #       each message has different reply_channel, so Group.discard is not testable
@@ -233,7 +240,7 @@ class ChannelReceiveTestCaseTwo(ChannelTestCase):
         self.assertEqual(result['header'], "get_bubble_list")
 
     def test_create_bubble_body_invalid_format(self):
-        message = {'header': 'create_bubble', 'body': {'parent': '1', 'location': '0'}}
+        message = {'header': 'create_bubble', 'body': {'parent': str(self.bubbleid), 'location': '0'}}
         self.client.send('websocket.receive', content={'order':1}, text=str(message))
         self.client.consume('websocket.receive')
         result = self.client.receive()
@@ -242,7 +249,7 @@ class ChannelReceiveTestCaseTwo(ChannelTestCase):
         self.assertEqual(result['body'], 'body does not follow format')
  
     def test_create_bubble_body_invalid_format(self):
-        message = {'header': 'create_bubble', 'body': {'parent': '1', 'location': '1', 'content': 'wow'}}
+        message = {'header': 'create_bubble', 'body': {'parent': str(self.bubbleid), 'location': '1', 'content': 'wow'}}
         self.client.send('websocket.receive', content={'order':1}, text=str(message))
         self.client.consume('websocket.receive')
         result = self.client.receive()
@@ -257,14 +264,14 @@ class ChannelReceiveTestCaseTwo(ChannelTestCase):
         sndClient.send_and_consume("websocket.connect", content={"text": {"header": "connect"}})
         sndClient.receive()
         sndClient.send('websocket.receive', content={'order':0}, 
-                text=str({"header": "open_document", "body": {"document_id": "1"}}))
+                text=str({"header": "open_document", "body": {"document_id": str(self.fstid)}}))
         sndClient.consume('websocket.receive')
         result = sndClient.receive()
         
         self.assertEqual(result['header'], 'open_document')
         self.assertEqual(result['accept'], 'True')
 
-        message = {'header': 'create_bubble', 'body': {'parent': '1', 'location': '0', 'content': 'wow'}}
+        message = {'header': 'create_bubble', 'body': {'parent': str(self.bubbleid), 'location': '0', 'content': 'wow'}}
         self.client.send("websocket.receive", content={'order':1}, text=str(message))
         self.client.consume('websocket.receive')
         result = self.client.receive()
