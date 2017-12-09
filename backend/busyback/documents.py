23c939ce8919b871fafbc9bb4cabe36326993c2c
Seungwoo Lee <lsw8075@gmail.com>
2017-12-09 16:43:35 +0900

    add document conntectors on cache

--- backend/busyback/documents.py
+++ backend/busyback/documents.py
@@ -3,6 +3,8 @@ from .users import fetch_user
 from .errors import *
 from django.utils import timezone
 from django.db import transaction
+from django.core.cache import cache
+import json
 
 @transaction.atomic
 def do_fetch_document(
@@ -52,3 +54,59 @@ def do_fetch_contributors(
 
     document = do_fetch_document(user_id, document_id)
     return list(document.contributors.all().values())
+
+def key_duser(document_id):
+    return 'Duser' + str(document_id)
+
+def do_user_connect_document(
+    user_id: int,
+    document_id: int
+    ):
+    key_doc = key_duser(document_id)
+    
+    with cache.lock('doclock' + str(document_id)):
+        connected_users = cache.get_or_set(key_doc, '[]')
+        connected_users = json.loads(connected_users)
+        connected_users.append(user_id)
+        connected_users = json.dumps(connected_users)
+
+        cache.set(key_doc, connected_users)
+
+def do_user_disconnect_document(
+    user_id: int,
+    document_id: int
+    ):
+    key_doc = key_duser(document_id)
+    with cache.lock('doclock' + str(document_id)):
+        connected_users = cache.get(key_doc)
+        connected_users = json.loads(connected_users)
+        connected_users.remove(user_id)
+        if len(connected_users) > 0:
+            connected_users = json.dumps(connected_users)
+            cache.set(key_doc, connected_users)
+        else:
+            cache.delete(key_doc)
+
+def do_get_connected_users_document(
+    user_id: int,
+    document_id: int
+    ):
+
+    key_doc = key_duser(document_id)
+    with cache.lock('doclock' + str(document_id)):
+        connected_users = cache.get(key_doc)
+        if connected_users is None:
+            return []
+        connected_users = json.loads(connected_users)
+        connected_users.sort()
+        return connected_users
+
+def do_clear_connected_users_document(
+    user_id: int,
+    document_id: int
+    ):
+    key_doc = key_duser(document_id)
+    with cache.lock('doclock' + str(document_id)):
+        connected_users = cache.get(key_doc)
+        if connected_users is not None:
+            cache.delete(key_doc)
