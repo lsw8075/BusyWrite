ee4ddcbc91f9ddf62d385288b6d1cf4f6ba70d3f
Dajung Je <ss01363@naver.com>
2017-12-14 22:48:25 -0800

    Implement server requesting methods
    on BubbleService of front.
    Note related logic is not implemented yet.

--- frontend/src/app/document/services/bubble.service.ts
+++ frontend/src/app/document/services/bubble.service.ts
@@ -29,6 +29,7 @@ export class BubbleService implements OnDestroy {
     private bubbleList: Array<Bubble> = [];
     private bubbleRoot: InternalBubble;
     // private user$: Observable<>;
+    private previousRequestId: Number;
 
     constructor(
             private _store: Store<Reducer.State>,
@@ -71,6 +72,7 @@ export class BubbleService implements OnDestroy {
         const data = msg;
         // let data = JSON.parse(msg);
         // TODO: check if data has appropriate elements
+        
         const command = data.header;
         const accept = data.accept;
         const body = data.body;
@@ -146,41 +148,57 @@ export class BubbleService implements OnDestroy {
         }
     }
 
-    public openDocument(documentId): Promise<void> {
+    public openDocument(documentId: number): Promise<void> {
         const m = {'header': 'open_document',
-                'body': {'document_id': documentId}};
+            'previous_request': 0,
+            'body': {'document_id': documentId}};
         console.log('send open_document');
         this._socket.send(m);
         return Promise.resolve(null);
     }
 
-    public closeDocument(documentId) {
+    public closeDocument(documentId: number) {
         const m = {'header': 'close_document',
-                'body': {'document_id': documentId}};
+            'previous_request': previousRequestId,
+            'body': {'document_id': documentId}};
         this._socket.send(m);
     }
 
     public getBubbleList() {
         const m = {'header': 'get_bubble_list',
-                'body': {'empty': 'empty'}};
+            'previous_request': previousRequestId,
+            'body': {'empty': 'empty'}};
         this._socket.send(m);
     }
 
-    public getSuggestBubbleList() {
+    public getSuggestBubbleList(bubbleId: number) {
+        const m = {'header': 'get_suggest_bubble_list',
+            'previous_request': previousRequestId,
+            'body': {'bubble_id': bubbleId}};
+        this._socket.send(m);
     }
 
-    public getCommentListForBubble() {
+    public getCommentListForBubble(bubbleId: number) {
+        const m = {'header': 'get_comment_list_for_bubble',
+            'previous_request': previousRequestId,
+            'body': {'bubble_id': bubbleId}};
+        this._socket.send(m);
     }
 
-    public getCommentListForSuggestBubble() {
+    public getCommentListForSuggestBubble(suggestBubbleId: number) {
+        const m = {'header': 'get_comment_list_for_suggest_bubble',
+            'previous_request': previousRequestId,
+            'body': {'bubble_id': suggestBubbleId}};
+        this._socket.send(m);
     }
 
-    public createBubbleMine(parentId: number, loc: number, content: string) {
+    public createBubble(parentId: number, loc: number, content: string) {
         // parent: parent bubble id (int)
         // location: nth child (0 ~ #currentchildbubble) (int)
         // content: (string)
         const m = {'header': 'create_bubble',
-            'body': {'parent': parentId,
+            'previous_request': previousRequestId,
+            'body': {'parent_id': parentId,
                 'location': loc,
                 'content': content}};
         this._socket.send(m);
@@ -188,34 +206,196 @@ export class BubbleService implements OnDestroy {
 
     public createSuggestBubble(bindedBubbleId: number, content: string) {
         const m = {'header': 'create_suggest_bubble',
+            'previous_request': previousRequestId,
             'body': {'binded_bubble': bindedBubbleId,
                 'content': content}};
         this._socket.send(m);
     }
 
-    public createCommentOnNormalBubble() {
+    public createCommentOnNormalBubble(bindedBubbleId: number, content: string) {
+        const m = {'header': 'create_comment_on_bubble',
+            'previous_request': previousRequestId,
+            'body': {'binded_bubble': bindedBubbleId,
+                'content': content}};
+        this._socket.send(m);
     }
 
-    public createCommentOnSuggestBubble() {
+    public createCommentOnSuggestBubble(bindedSuggestBubbleId: number, content: string) {
+        const m = {'header': 'create_comment_on_suggest_bubble',
+            'previous_request': previousRequestId,
+            'body': {'binded_suggest_bubble': bindedSuggestBubbleId,
+                'content': content}};
+        this._socket.send(m);
     }
 
     public startEdittingBubble(bubbleId: number) {
         const m = {'header': 'edit_bubble',
+            'previous_request': previousRequestId,
             'body': {'bubble_id': bubbleId}};
         this._socket.send(m);
     }
 
-    public edittingBubble() {
-        // TODO: back is working....
-    }
-
-    public finishEdittingBubble(bubble_id: number) {
-        const m = {'header': 'finish_editting_bubble',
-            'body': {'bubble_id': bubble_id}};
+    public updateEdittingBubble(bubbleId: number, content: string) {
+        const m = {'header': 'update_content_of_editting_bubble',
+            'previous_request': previousRequestId,
+            'body': {'bubble_id': bubbleId,
+                'content': content}};
         this._socket.send(m);
     }
 
-    public async splitBubble(bubbleId: number, contentList: Array<string>): Promise<Array<Bubble>> {
+    public finishEdittingBubble(bubbleId: number) {
+        const m = {'header': 'finish_editting_bubble',
+            'previous_request': previousRequestId,
+            'body': {'bubble_id': bubbleId}};
+        this._socket.send(m);
+    }
+
+    public discardEdittingBubble(bubbleId: number) {
+        const m = {'header': 'discard_editting_bubble',
+            'previous_request': previousRequestId,
+            'body': {'bubble_id': bubbleId}};
+        this._socket.send(m);
+    }
+
+    public releaseOwnershipOfBubble(bubbleId: number) {
+        const m = {'header': 'release_ownership_of_bubble',
+            'previous_request': previousRequestId,
+            'body': {'bubble_id': bubbleId}};
+        this._socket.send(m);
+    }
+
+    public editSuggestBubble(suggestBubbleId: number, content: string) {
+        const m = {'header': 'edit_suggest_bubble',
+            'previous_request': previousRequestId,
+            'body': {'suggest_bubble_id': suggestBubbleId,
+                'content': content}};
+        this._socket.send(m);
+    }
+
+    public deleteBubble(bubbleId: number) {
+        const m = {'header': 'delete_bubble',
+            'previous_request': previousRequestId,
+            'body': {'bubble_id': bubbleId}};
+        this._socket.send(m);
+    }
+
+    public hideSuggestBubble(suggestBubbleId: number) {
+        const m = {'header': 'hide_suggest_bubble',
+            'previous_request': previousRequestId,
+            'body': {'suggeest_bubble_id': suggestBubbleId}};
+        this._socket.send(m);
+    }
+
+    public showSuggestBubble(suggestBubbleId: number) {
+        const m = {'header': 'show_suggest_bubble',
+            'previous_request': previousRequestId,
+            'body': {'suggest_bubble_id': suggestBubbleId}};
+        this._socket.send(m);
+    }
+
+    public deleteCommentOnBubble(commentId: number) {
+        const m = {'header': 'delete_comment_on_bubble',
+            'previous_request': previousRequestId,
+            'body': {'comment_id': commentId}};
+        this._socket.send(m);
+    }
+
+    public deleteCommentOnSuggestBubble(commentId: number) {
+        const m = {'header': 'delete_comment_on_suggest_bubble',
+            'previous_request': previousRequestId,
+            'body': {'comment_id': commentId}};
+        this._socket.send(m);
+    }
+
+    public moveBubble(bubbleId: number, newParentId: number, newLocation: number) {
+        const m = {'header': 'move_bubble',
+            'previous_request': previousRequestId,
+            'body': {'bubble_id': bubbleId,
+                'new_parent_id': newParentId,
+                'new_location': newLocation}};
+        this._socket.send(m);
+    }
+
+    public wrapBubble(wrapBubbleIdList: Array<number>) {
+        const m = {'header': 'wrap_bubble',
+            'previous_request': previousRequestId,
+            'body': {'bubble_id_list': wrapBubbleIdList}};
+        this._socket.send(m);
+    }
+    
+    public popBubble(bubbleId: number) {
+        const m = {'header': 'pop_bubble',
+            'previous_request': previousRequestId,
+            'body': {'bubble_id': bubbleId}};
+        this._socket.send(m);
+    }
+
+    public splitInternalBubble(bubbleId: number, splitBubbleIdList: Array<number>) {
+        const m = {'header': 'split_internal_bubble',
+            'previous_request': previousRequestId,
+            'body': {'bubble_id': bubbleId,
+                'split_bubble_id_list': splitBubbleIdList}};
+        this._socket.send(m);
+    }
+
+    public splitLeafBubble(bubbleId: number, splitContentList: Array<String>) {
+        const m = {'header': 'split_leaf_bubble',
+            'previous_request': previousRequestId,
+            'body': {'bubble_id': bubbleId,
+                'split_content_list': [...content_list]}};
+        this._socket.send(m);
+    }
+
+    public mergeBubble(mergeBubbleIdList: Array<number>) {
+        const m = {'header': 'merge_bubble',
+            'previous_request': previousRequestId,
+            'body': {'merge_bubble_id_list': mergeBubbleIdList}};
+        this._socket.send(m);
+    }
+   
+    public flattenBubble(bubbleId: number) {
+        const m = {'header': 'flatten_bubble',
+            'previous_request': previousRequestId,
+            'body': {'bubble_id': bubbleId}};
+        this._socket.send(m);
+    }
+
+    public switchBubble(suggestBubbleId: number) {
+        const m = {'header': 'switch_bubble',
+            'previous_request': previousRequestId,
+            'body': {'suggest_bubble_id': suggestBubbleId}};
+        this._socket.send(m);
+    }
+
+    public voteOnSuggestBubble(suggestBubbleId: number) {
+        const m = {'header': 'vote_on_suggest_bubble',
+            'previous_request': previousRequestId,
+            'body': {'suggest_bubble_id': suggestBubbleId}};
+        this._socket.send(m);
+    }
+
+    public unvoteOnSuggestBubble(suggestBubbleId: number) {
+        const m = {'header': 'unvote_on_suggest_bubble',
+            'previous_request': previousRequestId,
+            'body': {'suggest_bubble_id': suggestBubbleId}};
+        this._socket.send(m);
+    }
+
+    public async _wrapBubble(wrapBubbleList: Array<number>): Promise<InternalBubble> {
+        await this.delay(1000);
+        return Promise.resolve(new InternalBubble(this._getTempBubbleId()));
+    }
+
+    public async _mergeBubble(mergeBubbleIds: Array<number>): Promise<Bubble> {
+        await this.delay(1000);
+        return Promise.resolve(new LeafBubble(this._getTempBubbleId()));
+    }
+    public async flattenBubble(bubble_id: number): Promise<Bubble> {
+        await this.delay(1000);
+        return Promise.resolve(new LeafBubble(this._getTempBubbleId()));
+    }
+
+    public splitBubble(bubbleId: number, contentList: Array<string>) {
         const splitBubbleList: Array<Bubble> = [];
         for (let content of contentList) {
             splitBubbleList.push(new LeafBubble(this._getTempBubbleId(), content));
@@ -223,47 +403,11 @@ export class BubbleService implements OnDestroy {
         return Promise.resolve(splitBubbleList);
     }
 
-    public deleteBubble(bubble_id: number) {
-        const m = {'header': 'delete_bubble',
-            'body': {'bubble_id': bubble_id}};
-        this._socket.send(m);
+
+    public _moveBubble(bubbleId: number, destBubbleId: number, isAbove: boolean) {
     }
 
-    public deleteSuggestBubble(){
-    }
-
-    public async wrapBubble(wrapBubbleList: Array<number>): Promise<InternalBubble> {
-        await this.delay(1000);
-        return Promise.resolve(new InternalBubble(this._getTempBubbleId()));
-    }
-
-    public async mergeBubble(mergeBubbleIds: Array<number>): Promise<Bubble> {
-        await this.delay(1000);
-        return Promise.resolve(new LeafBubble(this._getTempBubbleId()));
-    }
-
-    public splitLeafBubble(bubble_id: number, content_list: Array<String>) {
-        const m = {'header': 'split_leaf_bubble',
-            'body': {'bubble_id': bubble_id,
-                'content_list': [...content_list]}};
-        this._socket.send(m);
-    }
-
-    public async flattenBubble(bubble_id: number): Promise<Bubble> {
-        await this.delay(1000);
-        return Promise.resolve(new LeafBubble(this._getTempBubbleId()));
-    }
-
-    public moveBubble(bubbleId: number, destBubbleId: number, isAbove: boolean) {
-    }
-
-    public popBubble(bubble_id: number) {
-        const m = {'header': 'pop_bubble',
-            'body': {'bubble_id': bubble_id}};
-        this._socket.send(m);
-    }
-
-    public async createBubble(bubbleId: number, isAbove: boolean): Promise<Bubble> {
+    public async _createBubble(bubbleId: number, isAbove: boolean): Promise<Bubble> {
         await this.delay(1000);
         return Promise.resolve(new LeafBubble(this._getTempBubbleId(), 'new string'));
     }
