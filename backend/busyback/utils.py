12463fd9d53f59b586b43d0aabc63e9692dc5cbe
Seungwoo Lee <lsw8075@gmail.com>
2017-12-17 18:12:20 +0900

    delete document

--- backend/busyback/utils.py
+++ backend/busyback/utils.py
@@ -1,7 +1,30 @@
 from .models import *
+import json
 
 def create_normal(doc, content='', parent=None, location=0):
     return NormalBubble.objects.create(content=content, document=doc, location=location, parent_bubble=parent)
 
 def create_suggest(bind, content):
     return SuggestBubble.objects.create(content=content, normal_bubble=bind, hidden=False)
+
+def http_send(s, t, url, jd={}, result=False, login=False, csrf=None):
+    if login:
+        s.client.login(username='testuser2', password='5678')
+    response = None
+    url = '/api/' + url
+    if t != 'GET':
+        csrf = s.getCSRF()
+    if t == 'GET':
+        if jd != {}:
+            response = s.client.get(url, jd)
+        else:
+            response = s.client.get(url)
+    elif t == 'POST':
+        response = s.client.post(url, json.dumps(jd), content_type='application/json', HTTP_X_CSRFTOKEN=csrf)
+    elif t == 'PUT':
+        response = s.client.put(url, json.dumps(jd), content_type='application/json', HTTP_X_CSRFTOKEN=csrf)
+    elif t == 'DELETE':
+        response = s.client.delete(url, json.dumps(jd), content_type='application/json', HTTP_X_CSRFTOKEN=csrf)
+    if result:
+        s.result = json.loads(response.content.decode())
+    return response.status_code
